7eccdeccab02237528bec94ecdecd11640d7d11c
Aryawedha <gedearyawedha@unud.ac.id>
2026-01-13 15:38:47 +0800

    gsbgr

--- kontak.php
+++ kontak.php
@@ -21,7 +21,7 @@
 
 <section class="contact-section">
     <h1>Kontak Himpunan</h1>
-    <p class="subtitle">Hubungi pihak terkait untuk informasi peminjaman fasilitas Himpunan</p>
+    <p class="subtitle">Hubungi pihak Himpunan untuk informasi peminjaman fasilitas Himpunan</p>
 
     <div class="contact-container">
 
